c4bcee4607a91e625543846e77142158e96e7092
spblewis <spblewis@gmail.com>
2021-05-24 18:07:17 -0500

    Basic structure

--- src/index.js
+++ src/index.js
@@ -14,7 +14,24 @@ class Pomodoro extends React.Component {
 
     render() {
 
-        return <div>{this.state.session}</div>;
+        return (
+        
+            <div>
+                <div>
+                    <p id="break-label">Break Length</p>
+                    <p id="break-length">{this.state.break}</p>
+                    <button id="break-increment">Break++</button>
+                    <button id="break-decrement">Break--</button>
+                </div>
+                <div>
+                    <p id="session-label">Session Length</p>
+                    <p id="session-length">{this.state.session}</p>
+                    <button id="session-increment">Session++</button>
+                    <button id="session-decrement">Session--</button>
+                </div>
+            </div>
+        
+        );
 
     }
 }
