50f204bfb8efd56670d88e148c57e1517e7c036c
spblewis <spblewis@gmail.com>
2021-05-24 17:57:25 -0500

    initial setup

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+class Pomodoro extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            session: 25,
+            break: 5,
+        };
+    }
+
+
+    render() {
+
+        return <div>{this.state.session}</div>;
+
+    }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Pomodoro />,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
