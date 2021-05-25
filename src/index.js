3dcb8c171cbd07666919581f0be82f9584ba25bb
spblewis <spblewis@gmail.com>
2021-05-25 15:25:20 -0500

    Basic structural elements added

--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+
+
+
+
 class Pomodoro extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             session: 25,
             break: 5,
+            timeleft: "25:00", 
+            running: false
         };
     }
 
@@ -17,6 +23,10 @@ class Pomodoro extends React.Component {
         return (
         
             <div>
+                <div id="timer-label">Session</div>
+                <div id="time-left">{this.state.timeleft}</div>
+                <button id="start_stop">{this.state.running ? 'Stop' : 'Start'}</button>
+                <button id="reset">Reset</button>
                 <div>
                     <p id="break-label">Break Length</p>
                     <p id="break-length">{this.state.break}</p>
