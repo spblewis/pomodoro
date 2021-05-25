568ef2f212b92f2c6acf723c003c4ed19ecda818
spblewis <spblewis@gmail.com>
2021-05-25 18:56:02 -0500

    Redux state is wired in!  That was easier than I expected.

--- src/index.js
+++ src/index.js
@@ -1,41 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { connect } from 'react-redux';
+import appReducer from './reducers/reducer';
+const store = createStore(appReducer);
 
 
-
-
-
-class Pomodoro extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            session: 25,
-            break: 5,
-            timeleft: "60:00", 
-            running: false
-        };
-    }
-
-
-    render() {
+function Pomodoro({ appState }) {
 
         return (
         
             <div>
                 <div id="timer-label">Session</div>
-                <div id="time-left">{this.state.timeleft}</div>
-                <button id="start_stop">{this.state.running ? 'Stop' : 'Start'}</button>
+                <div id="time-left">{appState.timeleft}</div>
+                <button id="start_stop">{appState.running ? 'Stop' : 'Start'}</button>
                 <button id="reset">Reset</button>
                 <div>
                     <p id="break-label">Break Length</p>
-                    <p id="break-length">{this.state.break}</p>
+                    <p id="break-length">{appState.break}</p>
                     <button id="break-increment">Break++</button>
                     <button id="break-decrement">Break--</button>
                 </div>
                 <div>
                     <p id="session-label">Session Length</p>
-                    <p id="session-length">{this.state.session}</p>
+                    <p id="session-length">{appState.session}</p>
                     <button id="session-increment">Session++</button>
                     <button id="session-decrement">Session--</button>
                 </div>
@@ -43,10 +33,17 @@ class Pomodoro extends React.Component {
         
         );
 
-    }
 }
 
+const mapStateToProps = (state) => ({
+    appState: state,
+});
+
+const App = connect(mapStateToProps)(Pomodoro);
+
 ReactDOM.render(
-    <Pomodoro />,
+    <Provider store={store}>
+        <App />
+    </Provider>,
     document.getElementById('root')
 );
---
+++ src/reducers/reducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+    session: 25,
+    break: 5,
+    timeleft: "25:00", 
+    running: false
+};
+
+const appReducer = (state = initialState, action) => { 
+    return state;
+};
+
+export default appReducer;
