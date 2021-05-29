d44315ec8ec82ddd854fdae9fb5f62a63c235dac
spblewis <spblewis@gmail.com>
2021-05-29 14:55:22 -0500

    Lots of changes.  Getting lost with react-redux, redux thunk, all that.  I'll figure it out.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import { connect } from 'react-redux';
 import reducer from './reducers/reducer';
-const store = createStore(reducer);
+import thunk from 'redux-thunk';
+const store = createStore(reducer, applyMiddleware(thunk));
 
 
 function Pomodoro({ appState }) {
@@ -14,14 +15,13 @@ function Pomodoro({ appState }) {
         
             <div>
                 <div id="timer-label">Session</div>
-                <div id="time-left">{appState.currentTime}</div>
+                <div id="time-left">{appState.timeLeft.toString()}</div>
 
-                {/* heres a dummy div to temporarily display endTime.  
-                Remove when endTime is confirmed to function */}
-                <div id="end-time">{appState.endTime}</div>
-
-
-                <button id="start_stop">{appState.running ? 'Stop' : 'Start'}</button>
+                <div>{appState.running.toString()}</div>
+                <button 
+                    id="start_stop"
+                    onClick={!appState.running ? store.dispatch(start()) : store.dispatch(stop())}
+                >{appState.running ? 'Stop' : 'Start'}</button>
                 <button id="reset">Reset</button>
                 <div>
                     <p id="break-label">Break Length</p>
@@ -45,8 +45,31 @@ const mapStateToProps = (state) => ({
     appState: state,
 });
 
+//dispatch actions.  probably these need their own file eventually?
+const START = 'START';
+const STOP = 'STOP';
+const TICK = 'TICK';
+//start timer
+let timer = null;
+const start = () => (dispatch) => {
+    clearInterval(timer);
+    timer = setInterval(() => dispatch(tick()), 1000);
+    dispatch({type: START});
+    dispatch(tick());
+}
+//tick action creator
+const tick = () => ({ type: TICK });
+
+const stop = () => {
+
+    //wait, isn't this a side effect and shouldn't action creators be pure functions... Look this up
+    clearInterval(timer);
+    return ({ type: STOP });
+}
+
+
 const mapDispatchToProps = (dispatch) => ({
-  /* to do */
+  
 });
 
 const App = connect(mapStateToProps, mapDispatchToProps)(Pomodoro);
--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,23 +1,14 @@
 
-//Some global time variables to work with.  Not sure I'll use this method in the end.
-const currentTime = new Date(Date.now());
-const padTime = (num) => String(num).padStart(2, '0');
-const hours = padTime(currentTime.getHours() % 12 || 12);
-const minutes = padTime(currentTime.getMinutes());
-const seconds = padTime(currentTime.getSeconds());
-
-
 const initialState = {
     session: 25,
     break: 5,
-    timeleft: '25:00',
-    currentTime: `${hours}:${minutes}:${seconds}`,
-    endTime: null,
+    timeLeft: 25 * 60,
     running: false
 };
 
 const START = 'START';
 const STOP = 'STOP';
+const TICK = 'TICK';
 
 const appReducer = (state = initialState, action) => { 
 
@@ -25,11 +16,14 @@ const appReducer = (state = initialState, action) => {
         case START:
             return Object.assign({}, state, {
                 running: true,
-                endTime: currentTime + state.session * 60000,
             });
         case STOP:
             return Object.assign({}, state, {
-                running: false
+                running: false,
+            });
+        case TICK:
+            return Object.assign({}, state, {
+                timeLeft: state.timeLeft - 1,
             });
         default:
             return state;
